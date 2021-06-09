045f6a3217e8c67c9a669b6142206c2d4f88a49e
meldhuicque <83384147+meldhuicque@users.noreply.github.com>
2021-06-08 21:36:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const fs = require ('fs');
+const express = require ('express');
+const path = require ('path');
+const app = express();
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+app.use(express.static(__dirname + '/public'));
+app.listen(process.env.PORT || 3002, function() {
+    console.log("Servidor corriendo");
+})
+app.get('/', (req, res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+});
